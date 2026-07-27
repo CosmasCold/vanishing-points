f0e637010a68c9496492cc07e9bc7940180f3220
Gabriel <coldcosmas@gmail.com>
2026-07-27 13:31:08 -0400

    Whitelist Wikimedia Commons for image optimization

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["res.cloudinary.com", "api.mapbox.com"],
+    domains: [
+      "res.cloudinary.com",
+      "api.mapbox.com",
+      "upload.wikimedia.org",
+    ],
   },
   async headers() {
     return [
